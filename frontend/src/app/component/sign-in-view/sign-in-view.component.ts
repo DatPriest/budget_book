3976a99ddb22cf003474f46d59ac1f537662590e
Lukas Bullwinkel <lukas.bullwinkel@web.de>
2022-05-11 10:38:58 +0200

    Edit View
    - The pages Terms and Conditions, Imprint and Privacy filled with content and the .html and .css file created.
    MH-60 MH-61 MH-62

--- frontend/src/app/component/sign-in-view/sign-in-view.component.ts
+++ frontend/src/app/component/sign-in-view/sign-in-view.component.ts
@@ -29,7 +29,6 @@ export class SignInViewComponent implements OnInit {
 
   loginUser(signInForm: NgForm): void {
     this.router.navigate(['main']); // temp
-    this.alertService.successfulAlert("Herzlich willkommen!" ,  "Der Login war erfolgreich." ,  "success", 2500);
 
     this.hash = this.hashService.encrypt(signInForm.value.password);
     const signInData = new LoginUserModule(null, signInForm.value.email, this.hash);
