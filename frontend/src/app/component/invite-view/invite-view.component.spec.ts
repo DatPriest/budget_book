78487fd1f0a02af0c3803e25ce72456d647a57fb
Lukas Bullwinkel <lukas.bullwinkel@web.de>
2022-05-11 10:58:50 +0200

    General
    - Rename invite-link-view to invite-view.
    - Create a button in profile-view to open the invite Dialog.
    MH-81

--- frontend/src/app/component/invite-view/invite-view.component.spec.ts
+++ frontend/src/app/component/invite-view/invite-view.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InviteLinkViewComponent } from './invite-link-view.component';
+import { InviteLinkViewComponent } from './invite-view.component';
 
 describe('InviteLinkViewComponent', () => {
   let component: InviteLinkViewComponent;
---
+++ frontend/src/app/component/invite-view/invite-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-view',
+  templateUrl: './invite-view.component.html',
+  styleUrls: ['./invite-view.component.css']
+})
+export class InviteViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
