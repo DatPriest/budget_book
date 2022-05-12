602f735aef411ccf10b431f278e1c3ede7a537ac
Lukas Bullwinkel <lukas.bullwinkel@web.de>
2022-05-12 17:00:15 +0200

    Restore Invite View .css & spec.ts

---
+++ frontend/src/app/component/invite-view/invite-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InviteLinkViewComponent } from './invite-view.component';
+
+describe('InviteLinkViewComponent', () => {
+  let component: InviteLinkViewComponent;
+  let fixture: ComponentFixture<InviteLinkViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InviteLinkViewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InviteLinkViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
