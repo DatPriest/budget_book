37491b494d6bbeec8ddf189b8410e67beb663cdb
Lukas Bullwinkel <lukas.bullwinkel@web.de>
2022-06-02 12:44:24 +0200

    Edit Notification-View MH-69

--- frontend/src/app/component/notification-view/notification-view.component.ts
+++ frontend/src/app/component/notification-view/notification-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AlertService } from 'src/app/service/alert/alert.service';
+import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-notification-view',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationViewComponent implements OnInit {
 
-  constructor() { }
+  notificationForm: FormGroup;
+  constructor(public formBuilder: FormBuilder, public alertService: AlertService, public userService: UserService,
+    public dialogRef: MatDialogRef<NotificationViewComponent>) { }
+
+  save(notificationForm: NgForm) {
+    /*
+    this.userService.notification(notificationForm.value.acceptTerms).subscribe(data => {
+      this.alertService.successfulAlert("Erfolgreich geändert!",  "",  "success", 2500);
+      this.dialogRef.close();
+    });
+    */
+  }
 
   ngOnInit(): void {
+    this.notificationForm = this.formBuilder.group({
+      acceptTerms: ['', Validators.requiredTrue]
+    })
   }
 
 }
