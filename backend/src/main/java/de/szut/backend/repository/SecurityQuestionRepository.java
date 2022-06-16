57121f09d96076f0cfcca55c67aebee2e74e214e
Lukas Bullwinkel <66733054+bulli-03@users.noreply.github.com>
2022-06-16 11:11:06 +0200

    Fix backend

--- backend/src/main/java/de/szut/backend/repository/SecurityQuestionRepository.java
+++ backend/src/main/java/de/szut/backend/repository/SecurityQuestionRepository.java
@@ -1,9 +1,11 @@
 package de.szut.backend.repository;
 
+import de.szut.backend.exceptions.SecurityQuestionNotExists;
 import de.szut.backend.model.SecurityQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SecurityQuestionRepository extends JpaRepository<SecurityQuestion, Long> {
-    SecurityQuestion findByKey(String key);
-    boolean existsByKey(String key);
+public interface SecurityQuestionRepository extends JpaRepository<SecurityQuestion, Long>  {
+    SecurityQuestion findByKey(String key) throws SecurityQuestionNotExists;
+    boolean existsByKey(String key) throws SecurityQuestionNotExists;
+    SecurityQuestion findById(long id) throws SecurityQuestionNotExists;
 }
