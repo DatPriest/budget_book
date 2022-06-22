e4148ab7fd1598fb3ae2a5fd9cfc5c69546733c0
Lukas Bullwinkel <lukas.bullwinkel@web.de>
2022-06-22 21:54:45 +0200

    Edit history MH-119 #time 10m

--- frontend/src/app/model/history/history.module.ts
+++ frontend/src/app/model/history/history.module.ts
@@ -1,6 +1,10 @@
 export class HistoryModule {
   constructor(
-    public groupId: number,
-    public text: string
+    public id: number,
+    public actionName: string,
+    public additionalInformation: string,
+    public date_Created: string,
+    public date_Changed: string,
+    public groupId: number
   ) { }
 }
